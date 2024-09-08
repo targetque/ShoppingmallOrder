31b447855d5f81a471444a45cb22b7aa7b09e8fe
targetque <whitetorte99@gmail.com>
2024-09-08 20:55:55 +0900

    initial commit

---
+++ src/main/kotlin/com/example/shoppingmallorder/ShoppingmallOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.shoppingmallorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingmallOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<ShoppingmallOrderApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/shoppingmallorder/ShoppingmallOrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.shoppingmallorder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingmallOrderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
